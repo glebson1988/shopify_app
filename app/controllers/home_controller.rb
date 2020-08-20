61aa04e8f551ddf548e2f6403473d3e84ccc34a7
Gleb Grishakov <glebson1988@yandex.ru>
2020-08-20 15:19:30 +0300

    adding metafields of product to HomeController and show view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,16 @@ class HomeController < AuthenticatedController
 
   def show
     @product = ShopifyAPI::Product.find(params[:id])
+    @product_metafields = []
+    shopify_session = ShopifyAPI::Session.new(domain: 'gleb-test-store.myshopify.com',
+                                              api_version: '2020-07',
+                                              token: Shop.first.shopify_token)
+    ShopifyAPI::Base.activate_session(shopify_session)
+    headers = { 'X-Shopify-Access-Token' => shopify_session.token }
+    metafields = HTTParty
+                 .get('https://gleb-test-store.myshopify.com/admin/products/' + @product.id.to_s + '/metafields.json',
+                      headers: headers).values
+    @product_metafields.push(metafields)
   end
 
   def edit; end
